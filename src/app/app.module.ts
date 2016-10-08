b93ba6c4222ae1ab3366e76cee239a56e83627bf
The Buildbot <buildbot@angular2buch.de>
2016-10-08 21:10:45 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/7768f55393165c4b34c2303bc0213500d1c4fc14
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/166109872

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
