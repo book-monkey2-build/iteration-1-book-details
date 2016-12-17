a3c738a0ac1d399a655bac5dd9796f47e1499573
The Buildbot <buildbot@angular2buch.de>
2016-12-17 23:04:51 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/6e9fd8ab3ae3139e9b97cfda9bbde73b9aeab9af
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/184846990

--- styles.bundle.js
+++ styles.bundle.js
@@ -1,4 +1,4 @@
-webpackJsonp([1,2],{
+webpackJsonp([1,3],{
 
 /***/ 277:
 /***/ function(module, exports) {
@@ -446,4 +446,4 @@ module.exports = __webpack_require__(288);
 /***/ }
 
 },[558]);
-//# sourceMappingURL=styles.map
+//# sourceMappingURL=styles.bundle.map
