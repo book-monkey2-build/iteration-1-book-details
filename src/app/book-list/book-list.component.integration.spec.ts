13ee7967afceb03c13dc62e0b3aa8164e443b802
The Buildbot <buildbot@angular2buch.de>
2017-01-05 23:51:43 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/152b8033dedac8e53a088d82e865dd0e9bbd7b62
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/189362687

--- src/app/book-list/book-list.component.integration.spec.ts
+++ src/app/book-list/book-list.component.integration.spec.ts
@@ -1,4 +1,3 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
